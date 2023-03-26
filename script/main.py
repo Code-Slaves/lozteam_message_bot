bb12f28c5f7c6ba5c678ce662afd225b93700a3f
Code-Slaves <115467630+Code-Slaves@users.noreply.github.com>
2023-03-26 11:28:31 +0300

    Delete script directory

--- script/main.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('config.json') as f:
-    config = json.load(f)
-
-config['config']['message_param'] = 'False'
-		
-with open('config.json', 'w') as f:
-	config = json.dump(config, f)
